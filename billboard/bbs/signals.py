84c66222c5ffce3e9acd0875a09cdaf43a6ef8bb
stasmikulski <118758447+stasmikulski@users.noreply.github.com>
2023-04-26 00:03:25 +0300

    Add files via upload

--- billboard/bbs/signals.py
+++ billboard/bbs/signals.py
@@ -8,35 +8,52 @@ from datetime import datetime, timedelta
 one_week_ago = datetime.today() - timedelta(days=1)
 
 
-@receiver(m2m_changed, sender=Post.commentPost.through)
-def changing_categs(sender, instance, action, **kwargs):
-    if action == 'pre_add':
-        # This will give you the users BEFORE any removals have happened
-        print('pre_add:', instance.commentPost.all())
-    elif action == 'post_add':
-        # This will give you the users AFTER any removals have happened
-        print('post_add:', instance.commentPost.all())
-        print('CREATED:', instance.commentPost)
+@receiver(post_save, sender=Post)
+def post_created(instance, created, **kwargs):
+    if not created:
+        return
+    if created:
+        print('Post CREATED:', instance.categoryType)
         print(instance.title)
         print(instance.dateCreation)
+        print(instance.author)
 
-        email = User.objects.filter(
-        ).values_list('email', flat=True)
 
-        subject = f'New comment in post {instance.title}'
-        # subject = f'Новая статья в категории {instance.title}'
+@receiver(post_save, sender=Comment)
+def comment_created(instance, created, **kwargs):
+    if not created:
+        return
+    if created:
+        print('Comment CREATED:')
+        #print(instance.text)
+        #print(instance.dateCreation)
+        #print(instance.commentPost)
+        #print(instance.commentPost.id)
+        #print(instance.commentPost.title)
+        #print(instance.commentUser)
+        #print(instance.commentUser.id)
+        #print(instance.commentUser.email)
+
+        user = instance.commentUser
+        user_email = instance.commentUser.email
+        print(user, user_email)
+
+        email = user_email
+
+        subject = f'New comment in post {instance.commentPost.title}'
+        # subject = f'Новый комментарий к статье {instance.commentPost.title}'
     
         text_content = (
-            f'Заголовок: {instance.title}\n'
-            f'Ссылка на статью: http://127.0.0.1:8000{instance.get_absolute_url()}'
+            f'Заголовок: {instance.commentPost.title}\n'
+            f'Ссылка на статью: http://127.0.0.1:8000{instance.commentPost.get_absolute_url()}'
         )
         html_content = (
-            f'Заголовок: {instance.title}<br>'
-            f'<a href="http://127.0.0.1:8000{instance.get_absolute_url()}">'
+            f'Заголовок: {instance.commentPost.title}<br>'
+            f'<a href="http://127.0.0.1:8000{instance.commentPost.get_absolute_url()}">'
             f'Ссылка на статью</a>'
         )
-        for email in emails:
-            msg = EmailMultiAlternatives(subject, text_content, None, [email])
-            msg.attach_alternative(html_content, "text/html")
-            msg.send()
+        msg = EmailMultiAlternatives(subject, text_content, None, [email])
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
+
 
